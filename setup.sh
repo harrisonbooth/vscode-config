e213ccf82b582afad6b20a4fa6d1f75b262dd402
Harrison Booth <harrisonboothh@gmail.com>
2019-10-02 16:45:40 +0100

    Add fira font install and vscode extension installs

---
+++ setup.sh
@@ -0,0 +1,7 @@
+brew cask install homebrew/cask-fonts/font-fira-code
+
+code --install-extension dracula-theme.theme-dracula
+code --install-extension pkief.material-icon-theme
+code --install-extension rebornix.ruby
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension octref.vetur
